7d98a8ff589662a6159841f4bc0677fc9abfe025
pyz1m <samuelyariwake@gmail.com>
2024-09-16 19:05:24 -0300

    oficinas add

---
+++ src/routes/Pages/Oficinas/Oficinas.tsx
@@ -0,0 +1,19 @@
+export const oficinas = [
+    {nome: 'Oficina A', localizacao: 'Rua C, 789', contato: 'Maria Silva', status: 'Aberto' },
+  {nome: 'Oficina B', localizacao: 'Avenida D, 101', contato: 'Carlos Souza', status: 'Fechado' },
+  { nome: 'Oficina C', localizacao: 'Rua E, 112', contato: 'Ana Pereira', status: 'Aberto' },
+  { nome: 'Oficina D', localizacao: 'Avenida F, 131', contato: 'Pedro Lima', status: 'Fechado' },
+  { nome: 'Oficina E', localizacao: 'Rua G, 415', contato: 'João Alves', status: 'Aberto' },
+  { nome: 'Oficina F', localizacao: 'Avenida H, 161', contato: 'Fernanda Costa', status: 'Fechado' },
+  { nome: 'Oficina G', localizacao: 'Rua I, 718', contato: 'Lucas Martins', status: 'Aberto' },
+  { nome: 'Oficina H', localizacao: 'Avenida J, 192', contato: 'Juliana Rocha', status: 'Fechado' },
+  { nome: 'Oficina I', localizacao: 'Rua K, 223', contato: 'Ricardo Mendes', status: 'Aberto' },
+  { nome: 'Oficina J', localizacao: 'Avenida L, 242', contato: 'Patrícia Gonçalves', status: 'Fechado' },
+  { nome: 'Oficina K', localizacao: 'Rua M, 525', contato: 'Renato Oliveira', status: 'Aberto' },
+  { nome: 'Oficina L', localizacao: 'Avenida N, 262', contato: 'Camila Santos', status: 'Fechado' },
+  { nome: 'Oficina M', localizacao: 'Rua O, 272', contato: 'Eduardo Barros', status: 'Aberto' },
+  { nome: 'Oficina N', localizacao: 'Avenida P, 282', contato: 'Larissa Teixeira', status: 'Fechado' },
+  { nome: 'Oficina O', localizacao: 'Rua Q, 292', contato: 'Marcelo Ferreira', status: 'Aberto' },
+  { nome: 'Oficina P', localizacao: 'Avenida R, 303', contato: 'Sandra Nunes', status: 'Fechado' },
+  { nome: 'Oficina Q', localizacao: 'Rua S, 313', contato: 'Thiago Ribeiro', status: 'Aberto' }
+  ];
